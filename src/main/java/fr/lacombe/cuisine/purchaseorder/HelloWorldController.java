edec24de857649b627c2fd2df9d4af7dba3a9049
Jean-FrancoisVo <jeanfrancoisvo7@gmail.com>
2018-12-20 09:40:49 +0100

    Hello world

---
+++ src/main/java/fr/lacombe/cuisine/purchaseorder/HelloWorldController.java
@@ -0,0 +1,12 @@
+package fr.lacombe.cuisine.purchaseorder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello";
+    }
+}
